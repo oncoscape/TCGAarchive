079b6476b355b54c7b957e1944b3433e0291a0cd
Jenny Zhang <canaantt@gmail.com>
2016-10-19 17:26:03 -0700

    separate the script to generate ptList.json

--- test/toolTesting/generatePatientListForDisease.js
+++ test/toolTesting/generatePatientListForDisease.js
@@ -36,34 +36,20 @@ var diseases = [];
 
 connection.once('open', function(){
 
-    lookupByDisease = connection.db.collection("lookup_oncoscape_datasources").find();
-    lookupByDisease.each(function(err, item){
-          if(item != null){
-            console.log(item['disease']);
-            diseases.push(item['disease']);
-            disease_arr.push(item);
-          }     
-    }); 
-    console.log("test0");
-    disease_arr.forEach(function(d){
-      console.log("test1");
-      var ptIDs = [];
-      if(('clinical' in d)&&('patient' in d['clinical'])){
-        var pt = connection.db.collection(d['clinical']['patient']).find({},{'patient_ID':true});
-        pt.each(function(err, item){
-          if(item != null)
-          ptIDs.push(item['patient_ID']);
-        });
-      }
-      ptList[d.disease] = ptIDs;
-    });
+    lookupByDisease = connection.db.collection("lookup_oncoscape_datasources").find().toArray();
 
-    console.log("test2");
-    Object.keys(ptList).forEach(function(k){
-      console.log("test3");
-       ptList[k] = u.uniq(ptList[k]);
+    lookupByDisease.then(function(obj){
+      obj.forEach(function(d){
+        var ptIDs = [];
+        if(('clinical' in d)&&('patient' in d['clinical'])){
+          var pt = connection.db.collection(d['clinical']['patient']).find({},{'patient_ID':true}).toArray();
+          pt.then(function(value){
+            ptIDs = value.map(function(v){return v['patient_ID'];});
+            ptList[d.disease] = u.uniq(ptIDs);
+          });
+        }
+      });
     });
-    console.log("test4");
     jsonfile.writeFile('ptList.json', ptList, {spaces:4});
    
 });
