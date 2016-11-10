0003df5a41e15577ee2069eec02cb3897382c614
Jenny Zhang <canaantt@gmail.com>
2016-11-10 15:55:26 -0800

    partially completed the mds

--- test/report/checkingPcaMdsCollectionNaming.js
+++ test/report/checkingPcaMdsCollectionNaming.js
@@ -15,8 +15,8 @@ const helper = require("../testingHelper.js");
 var elem = [];
 var db, collection, collections, collection_names;
 var lookup_table;
-var genesets;
-
+var genesets, genesetsStrings;
+var mds_sub_arrays;
 var onerror = function(e){
     console.log(e);
 };
@@ -31,7 +31,8 @@ co(function *() {
   lookup_table = yield collection.find({}).toArray();
   collection = yield comongo.db.collection(db, 'hg19_genesets_hgnc_import');
   genesets = yield collection.distinct('name');
-  
+  genesetsStrings = yield genesets.map(function(g){ return g.toLowerCase().replace(/\s/g, '');});
+
   var molComboForCalculated = function(lookupItem){
     // by data sources
     var result = [];
@@ -39,16 +40,23 @@ co(function *() {
     var molGrpBySource = u.groupBy(lookupItem.molecular, "source");
 
     Object.keys(molGrpBySource).forEach(function(k){
-      if(molGrpBySource.broad.map(function(m){return m.type;}).includesArray(['mut01','cnv']).includes.length==2){
-        //can calculate MDS
-       var str = lookupItem.disease + "_mds_" + k + "_mds-"   ;
+      console.log(k);
+      if(molGrpBySource[k].map(function(m){return m.type;}).includesArray(['mut01','cnv']).includes.length==2){
+        //can calculate MDS  
+       mds_sub_arrays = genesetsStrings.map(function(g){ return lookupItem.disease + "_mds_" + k + "_mds-" + g + "-cnv-mut01-" + k;});
+       //console.log(mds_sub_arrays);
+       result = result.concat(mds_sub_arrays);
       }
+      
+
     });
+    return result;
   }
  lookup_table.forEach(function(l){
    var molecularCombinations = molComboForCalculated(l);
-   var currentCalculatedCollections = l.calculated.map(function(m){return m.collection;});
-   console.log(molecularCombinations.arraysCompareV2(currentCalculatedCollections));
+   console.log(molecularCombinations);
+  //  var currentCalculatedCollections = l.calculated.map(function(m){return m.collection;});
+  //  console.log(molecularCombinations.arraysCompareV2(currentCalculatedCollections));
  });
 //   collections = yield comongo.db.collections(db);
 //   collection_names = collections.map(function(c){return c['s']['name'];});
