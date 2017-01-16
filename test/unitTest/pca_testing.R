7a22e18f1fd7552fac706bc38d4133e5827360f1
canaantt <canaantt@gmail.com>
2017-01-16 15:59:29 -0800

    still working on getting raw

--- test/unitTest/pca_testing.R
+++ test/unitTest/pca_testing.R
@@ -9,8 +9,15 @@ mongo <- connect.to.mongo()
 ## creates mongo.manifest and mongo.lookup connections
  
 ## Example queries
-  lgg_mut = mongo.manifest$find(toJSON(list(collection="tcga_lgg_mutation_broad_ucsc-xena"), auto_unbox = T))
-  con <- mongo("lookup_dataTypes", db=db, url=host)
-  dataTypes_mol = con$distinct("dataType", '{"$and":[{"schema":"hugo_sample"},{"class": {"$in":["cnv_thd", "mut", "mut01"]}}]}')
-rm(con)
+#   lgg_mut = mongo.manifest$find(toJSON(list(collection="tcga_lgg_mutation_broad_ucsc-xena"), auto_unbox = T))
+#   con <- mongo("lookup_dataTypes", db=db, url=host)
+#   dataTypes_mol = con$distinct("dataType", '{"$and":[{"schema":"hugo_sample"},{"class": {"$in":["cnv_thd", "mut", "mut01"]}}]}')
+# rm(con)
 
+## Obtain pca score and loading
+acc_cluster = mongo("acc_cluster", db=db, url=host)
+acc_cluster_pca = acc_cluster$find(toJSON(list(dataType="PCA"), auto_unbox = T))
+# length(acc_cluster_pca) #12
+# str(acc_cluster_pca,max.level=1)
+
+## Obtain raw data 
