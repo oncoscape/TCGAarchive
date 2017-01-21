b7f0f83a58819dc133ef7b9e6071d0be01fe4a28
canaantt <canaantt@gmail.com>
2017-01-20 18:37:51 -0800

    updated

--- survival_testing.js
+++ survival_testing.js
@@ -13,6 +13,16 @@ rm(con)
 brs = brain_pt[order(brain_pt$days_to_death),c(2,16,18)]
 brs$SurvObj <- with(brs, Surv(days_to_death, status_vital=="DEAD"))
 km.as.one <- survfit(SurvObj ~ 1, data = brs, conf.type ="log-log")
-brs_sample = tail(brs[order(brs$patient_ID),], n=29)
+random_pos = sample(1:nrow(brs), 20)
+random_IDs = brs$patient_ID[random_pos]
+brs_sample = brs[random_pos,]
 km.as.sp <- survfit(SurvObj ~ 1, data = brs_sample, conf.type ="log-log")
+pdf(file="survival/brs_rnd20.pdf")
+plot(km.as.sp)
+dev.off()
 
+// random_IDs
+// TCGA-06-0744,TCGA-S9-A6WH,TCGA-06-1802,TCGA-08-0244,TCGA-15-1444,
+// TCGA-QH-A6CY,TCGA-06-0165,TCGA-E1-5303,TCGA-HT-7860,TCGA-DU-5851,
+// TCGA-26-5136,TCGA-CS-4941,TCGA-CS-4944,TCGA-16-1060,TCGA-12-1088,
+// TCGA-RY-A83Y,TCGA-27-2528,TCGA-76-4932,TCGA-RY-A843,TCGA-P5-A72U
