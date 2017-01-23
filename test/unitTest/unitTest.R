0ff1a2e4cd5ba2c7f6f6b563a59cb3415fa0b318
canaantt <canaantt@gmail.com>
2017-01-23 11:47:14 -0800

    move the survival test script to the right dir

--- test/unitTest/unitTest.R
+++ test/unitTest/unitTest.R
@@ -2,3 +2,6 @@ source("common.R")
 db <- "tcga"
 host<- paste("mongodb://",user,":",password,"@oncoscape-dev-db1.sttrcancer.io:27017,oncoscape-dev-db2.sttrcancer.io:27017,oncoscape-dev-db3.sttrcancer.io:27017",sep="")
 mongo <- connect.to.mongo()
+
+# Brain(gbm, lgg), Breast, Lung(luad&lusc), Head&Neck, Prostate
+
